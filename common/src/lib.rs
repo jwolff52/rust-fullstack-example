6f00499a44bb66a946138f9a11b253ea1c12c5af
James Wolff <james.wolff0@gmail.com>
2023-02-24 17:49:40 +0000

    Common library

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+pub struct Owner {
+    pub id: i32,
+    pub name: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct OwnerRequest {
+    pub name: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct OwnerResponse {
+    pub id: i32,
+    pub name: String,
+}
+
+impl OwnerResponse {
+    pub fn of(owner: Owner) -> OwnerResponse {
+        OwnerResponse { id: owner.id, name: owner.name }
+    }
+}
+
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+pub struct Pet {
+    pub id: i32,
+    pub name: String,
+    pub owner_id: i32,
+    pub animal_type: String,
+    pub color: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct PetRequest {
+    pub name: String,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub struct PetResponse {
+    pub id: i32,
+    pub name: String,
+    pub animal_type: String,
+    pub color: Option<String>,
+}
+
+impl PetResponse {
+    pub fn of(pet: Pet) -> PetResponse {
+        PetResponse { id: pet.id, name: pet.name, animal_type: pet.animal_type, color: pet.color }
     }
 }
