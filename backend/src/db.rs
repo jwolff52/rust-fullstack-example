b5dea67e6a6d96813cf1ee521655519951c6e919
James Wolff <james.wolff0@gmail.com>
2023-02-25 22:58:24 -0600

    Moved db.sql

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -12,7 +12,7 @@ type Result<T> = std::result::Result<T, error::Error>;
 const DB_POOL_MAX_OPEN: u64 = 32;
 const DB_POOL_MAX_IDLE: u64 = 8;
 const DB_POOL_TIMEOUT_SECONDS: u64 = 15;
-const INIT_SQL: &str = "backend/src/db.sql";
+const INIT_SQL: &str = "backend/db.sql";
 
 pub async fn init_db(db_pool: &DBPool) -> Result<()> {
     let init_file = fs::read_to_string(INIT_SQL)?;
